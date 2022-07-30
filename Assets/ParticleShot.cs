cb5d65bc39344c847460844be76d8844f03d2414
AliHashish <67716981+AliHashish@users.noreply.github.com>
2022-07-30 21:54:01 +0200

    Player Particle Shot Done
    5aletha msh btedrab el player, bs el downside eny m7tag a3ml wa7da mo5tlfa lel enemies kda

--- Assets/ParticleShot.cs
+++ Assets/ParticleShot.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class ParticleShot : MonoBehaviour
 {
+    // el particle shot dyh bta3t el player bs
+    // el enemy h3mlo particle shot mo5tlfa
+
+
     public GameObject hitEffect;
 
     // Start is called before the first frame update
@@ -21,10 +25,13 @@ public class ParticleShot : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other) 
     {
-        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
-        // bn-instantiate el hit effect, w bn3mlo store fy object, 3lshan n3mlo destroy b3deeha
-        Destroy(effect, 0.41f);    // bn3ml destroy bs b3deeha b 5 seconds
-        Destroy(gameObject);    // bn3ml destroy lel bullet
-        
+        // 3ayez at2kd eny m5abatesh el player tho
+        if (!(other.gameObject.CompareTag("Player")))
+        {
+            GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            // bn-instantiate el hit effect, w bn3mlo store fy object, 3lshan n3mlo destroy b3deeha
+            Destroy(effect, 0.41f);    // bn3ml destroy bs b3deeha b 5 seconds
+            Destroy(gameObject);    // bn3ml destroy lel bullet
+        }
     }
 }
