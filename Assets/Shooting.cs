548aee360facf1a79281446140f2c45e2fa1a275
AliHashish <67716981+AliHashish@users.noreply.github.com>
2022-07-30 02:13:21 +0200

    n2lt el shooting script lel gun

--- Assets/Shooting.cs
+++ Assets/Shooting.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class Shooting : MonoBehaviour
 {
 
-    public Transform firePoint;
+    // public Transform firePoint;
     public GameObject bulletPrefab;
 
-    public float bulletForce = 2f;         // sor3et el bullet
+    public float bulletForce = 1.25f;         // sor3et el bullet
 
     // Start is called before the first frame update
     void Start()
@@ -28,14 +28,14 @@ public class Shooting : MonoBehaviour
     // the function that handles the shooting logic
     void Shoot()
     {
-        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        GameObject bullet = Instantiate(bulletPrefab, this.GetComponent<Transform>().position, this.GetComponent<Transform>().rotation);
         // bn3ml object mn el bulletPrefab el 7atenaha, fl position dh, bl rotation dyh
         // w bn5azeno fy variable
         
         // bs e7na 3ayzeen n-access el rigidbody bta3 el bullet, fa hnst3ml getcomponent
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();    // bn5azen el rigidbody
         // hndeelo force b2a
-        rb.AddForce(firePoint.right * bulletForce, ForceMode2D.Impulse);
+        rb.AddForce(this.GetComponent<Transform>().right * bulletForce, ForceMode2D.Impulse);
         // ba7aded el direction w no3 el force
     }
 }
