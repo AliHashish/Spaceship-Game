26e5d327c42bea0195c0f821f1dc2910fc1fa359
AliHashish <67716981+AliHashish@users.noreply.github.com>
2022-08-01 18:05:53 +0200

    damage zone update
    damage zone dlw2ty msh btedrab lw el shield on

--- Assets/DamageZone.cs
+++ Assets/DamageZone.cs
@@ -20,8 +20,8 @@ public class DamageZone : MonoBehaviour, IZone
 
     public void ZoneEffect()
     {
-        // h7tag azwd hena check 3l enemy lw kda
-        if (playerVars.health > playerVars.maxHealth * 0.5)     // mt2alelsh lw a2al mn noso already
+        // h7tag azwd hena check 3l enemy lw kda, 3lshan howa kaman yetdereb
+        if (!playerVars.shield && playerVars.health > playerVars.maxHealth * 0.5)     // mt2alelsh lw a2al mn noso already
         {
             playerVars.health -= playerVars.health * 0.00015f;   // kol sanya by2alel 7aba, bs el value bt2el kol shwya
         }
