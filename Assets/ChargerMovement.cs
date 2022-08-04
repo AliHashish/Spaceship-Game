8efbd6805578413ab2ed832fbf4f6c94b0b0bc7e
AliHashish <67716981+AliHashish@users.noreply.github.com>
2022-08-04 16:47:41 +0200

    Charger Done

--- Assets/ChargerMovement.cs
+++ Assets/ChargerMovement.cs
@@ -13,17 +13,22 @@ public class ChargerMovement : MonoBehaviour
     private ChargerState state;
     SpriteRenderer rend;
     Color C;
+    public float chargingSpeed = 4f;                          // force by which enemy charges at player
     public float health = 150f;     // initially, health = 150
     public float maxHealth = 150f;     // initially, max health = 150
     public ParticleShootingLogic playerVars;         // To access some variables
-    public float transitionTime = 4f;       // byo3od ad eih 3la ma yen2el mn state lel tanya
+    public float transitionTime = 1.5f;       // byo3od ad eih 3la ma yen2el mn state lel tanya
     float nextAttackTime = 0f;
+    public float damage = 15f;
 
     public GameObject player;
     // public GameObject enemy;
     public float attackRange = 1.3f;        // y2dr ydrb mn 3la bo3d ad eih
     public float movingSpeed = 1f;         // el sor3a el hyemshy byha
     public Rigidbody2D rb;              // byshawer 3l rigidbody bta3 el enemy
+
+    private Vector2 lookDir;
+    private float angle;
     // Start is called before the first frame update
     void Start()
     {
@@ -48,7 +53,7 @@ public class ChargerMovement : MonoBehaviour
         {
         case ChargerState.Idle:
             // hy-check lw el player da5al el radius bta3o
-            Debug.Log("Idle");
+            // Debug.Log("Idle");
             if (Vector3.Distance(player.transform.position, transform.position) < attackRange)
             {
                 state = ChargerState.Transition;
@@ -59,21 +64,22 @@ public class ChargerMovement : MonoBehaviour
 
         case ChargerState.Transition:
             // hybos lel player, w ystna shwya, w lono yo3od yt8yr, 3lshan el player y3rf eno hy-attacko, b3dein y5osh fl chargin state
-            Debug.Log("Transition");
+            // Debug.Log("Transition");
             // bybos lel player
-            Vector2 lookDir = (Vector2) player.transform.position - rb.position;       // el far2 ma bein el enemy wl player howa el vector mn el player lel enemy
-            float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg + 90f;;                                   
+            lookDir = (Vector2) player.transform.position - rb.position;       // el far2 ma bein el enemy wl player howa el vector mn el player lel enemy
+            angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg + 90f;;                                   
             rb.rotation = angle;
 
 
             // Color changes gradually
-            if (C.a < 1f)
+            if (C.a > 0.5f)
             {
-                Debug.Log($"Before: C.a = {C.a}");
-                C.a += 0.01f;
+                // Debug.Log($"Before: C.a = {C.a}");
+                C.a -= 0.01f;
                 rend.material.color = C;
-                Debug.Log($"After: C.a = {C.a}");
+                // Debug.Log($"After: C.a = {C.a}");
             }
+            
 
             // hyo3od lono yt8yr, l7d ma wa2t el transition (4 swany mthln) ye5las, sa3etha en2el lel Charge state
             if (Time.time >= nextAttackTime)
@@ -91,24 +97,28 @@ public class ChargerMovement : MonoBehaviour
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         case ChargerState.Charge:
             // hy-charge ne7yet el player, w lw 5abato hyna2aso w yrg3 lel state le idle b3deeha, w lono yrg3 zy ma kan
-            Debug.Log("Charge");
+            // Debug.Log("Charge");
 
 
             
             // color returns gradually
-            if (C.a > 0.5f)
+            if (C.a < 1f)
             {
-                Debug.Log($"Before: C.a = {C.a}");
-                C.a -= 0.01f;
+                // Debug.Log($"Before: C.a = {C.a}");
+                C.a += 0.01f;
                 rend.material.color = C;
-                Debug.Log($"After: C.a = {C.a}");
+                // Debug.Log($"After: C.a = {C.a}");
             }
             
+            lookDir = (Vector2) player.transform.position - rb.position;       // el far2 ma bein el enemy wl player howa el vector mn el player lel enemy
+            angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg + 90f;;                                   
+            rb.rotation = angle;
 
             if (Time.time >= nextAttackTime)
             {
-                // Call Coroutine that changes colour gradually, and waits a while
-                // StartCoroutine(RegainColour(2));
+                Debug.Log("Charging dlw2ty");
+                // charge 3l player
+                gameObject.GetComponent<Rigidbody2D>().AddForce(this.transform.up * -chargingSpeed, ForceMode2D.Impulse);
                 
                 nextAttackTime = Time.time + transitionTime;
                 state = ChargerState.Idle;
@@ -146,6 +156,10 @@ public class ChargerMovement : MonoBehaviour
         {
             health -= playerVars.damage;
         }
+        else if (bullet.gameObject.CompareTag("Player"))
+        {
+            bullet.transform.GetComponent<PlayerPickUp>().health -= damage;
+        }
 
     }
 
@@ -162,4 +176,5 @@ public class ChargerMovement : MonoBehaviour
             // in that case, we send the gameObject to know exactly which enemy it is
         }
     }
+    
 }
