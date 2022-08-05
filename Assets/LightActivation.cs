07da2f97ffda327c9b7a4a3c29feaa69cdfa4245
AliHashish <67716981+AliHashish@users.noreply.github.com>
2022-08-05 14:44:10 +0200

    zwdt shwyt lights kaman

--- Assets/LightActivation.cs
+++ Assets/LightActivation.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class LightActivation : MonoBehaviour
 {
+    GameObject player = null;
+    Light2D zoneLight;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+        zoneLight = GetComponent<Light2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Vector3.Distance(player.transform.position, transform.position) <+ zoneLight.pointLightInnerRadius)
+        {
+            zoneLight.intensity= Mathf.PingPong(Time.time, 1.5f);
+        }
     }
 }
