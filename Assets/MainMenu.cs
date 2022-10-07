ec900c54fca5dbfc34de2f03a05de63dcba6747a
AliHashish <67716981+AliHashish@users.noreply.github.com>
2022-10-07 21:14:34 +0200

    Main menu done

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;          // added to control scene management
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        // ht-load el scene elly 3leh el dor b3deeha
+        // btzabat el edwar mn el scene manager
+        // btro7lo mn unity, file >> Build Settings
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void LevelSelect()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Level(int level)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + level);
+    }
+
+    public void QuitGame()
+    {
+        // Debug.Log("Quitting Game! Bueno Adios");
+        Application.Quit();     // dh msh byezhar fl unity editor, fa zwd debug lyk
+                                // hysht8l lama t3ml build lel game tho
+    }
+}
